ab99a3db2791b6badb776745bf6468bc1cbb68bf
Jason Chan <jason741852@gmail.com>
2017-11-12 01:00:37 -0800

    First commit

---
+++ Assets/Assets/Scripts/Movement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+using UnityEngine;
+
+public class Movement : EventTrigger
+{
+    private Vector2 speed = new Vector2(4f, 0);
+    private bool buttonDown = false;
+    private Rigidbody2D rb;
+    private float screenWidth;
+    private float spaceshipWidth;
+
+    Vector2 playerPosScreen;
+
+    protected Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        screenWidth = Screen.width;
+
+        spaceshipWidth = GameObject.Find("Player").GetComponent<Renderer>().bounds.size.x;
+        
+        rb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+
+    }
+    void Update()
+    {
+        playerPosScreen = Camera.main.WorldToScreenPoint(rb.position);
+        if (buttonDown)
+        {
+            if(gameObject.name == "RightButton")
+            {
+                if (playerPosScreen.x < screenWidth - spaceshipWidth)
+                {
+                    var pos = rb.position + speed * Time.deltaTime;
+                    pos.x = Mathf.Clamp(pos.x, -screenWidth, screenWidth);
+                    rb.MovePosition(pos);
+                }
+            }
+            else if (gameObject.name == "LeftButton")
+            {
+                if (playerPosScreen.x > spaceshipWidth)
+                {
+                    var pos = rb.position - speed * Time.deltaTime;
+                    pos.x = Mathf.Clamp(pos.x, -screenWidth, screenWidth);
+                    rb.MovePosition(pos);
+                }
+            }
+
+        }
+    }
+
+    public override void OnPointerDown(PointerEventData data)
+    {
+        Debug.Log("OnPointerDown called.");
+        buttonDown = true;
+    }
+
+    public override void OnPointerUp(PointerEventData data)
+    {
+        buttonDown = false;
+    }
+
+
+}
---
+++ Assets/Assets/Scripts/ObstacleSpawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleSpawn : MonoBehaviour {
+    public GameObject obstacle;
+    public int maxPlatforms = 5;
+    public float horizontalMin = -2.9f;
+    public float horizontalMax = 2.9f;
+    public float verticalMin = 3f;
+    public float verticalMax = 6f;
+    public Vector2 speed = new Vector2(0, -3f);
+
+    private float screenWidth = Screen.width;
+    private Rigidbody2D rb;
+
+    private Vector2 originalPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        originalPosition = transform.position;
+        Spawn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var pos = rb.position + speed * Time.deltaTime;
+
+        rb.MovePosition(pos);
+
+    }
+
+    void Spawn()
+    {
+        for(int i=0; i< maxPlatforms; i++)
+        {
+            Vector2 randomPosition = new Vector2(Random.Range(-2.9f, 2.9f), originalPosition.y + Random.Range(3f, 6f));
+            GameObject obst = (GameObject)Instantiate(obstacle, randomPosition, Quaternion.identity);
+            Transform t = obst.transform;
+            t.parent = transform;
+
+            originalPosition = randomPosition;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Game Over?");
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GamePlay");
+        }
+    }
+}
